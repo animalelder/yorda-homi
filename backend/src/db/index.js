b9dcd8040993bac43365800fc403a2ccd3d783f3
animalelder <wllearned@icloud.com>
2025-01-22 18:49:56 -0800

    added SSL to db client props

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,4 +1,4 @@
-const pg = require("pg");
+const pg = require('pg');
 
 const client = new pg.Client({
   host: process.env.PGHOST,
@@ -6,11 +6,11 @@ const client = new pg.Client({
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+  ssl: process.env.SSL ? { rejectUnauthorized: false } : false,
 });
 client
   .connect()
-  .then(() => console.log("Connected to PostgreSQL successfully!"))
-  .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
+  .then(() => console.log('Connected to PostgreSQL successfully!'))
+  .catch((e) => console.log(`Error connecting to Postgres server:\n${e}`));
 
 module.exports = client;
