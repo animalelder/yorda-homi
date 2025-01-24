4dfca74266f4f1d0fa3f1c89e83c42c9a50c9704
animalelder <wllearned@icloud.com>
2025-01-24 11:28:48 -0800

    Adjust DB connection to use string

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,13 +1,8 @@
 const pg = require('pg');
 
-const client = new pg.Client({
-  host: process.env.PGHOST,
-  name: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-  ssl: process.env.SSL ?? 'require',
-});
+const config = { connectionString: process.env.DB_URI };
+
+const client = new pg.Client(config);
 
 client
   .connect()
