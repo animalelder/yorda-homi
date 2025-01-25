75d52528953313a5c44cbc8241e55392a1a13fda
Yordanos gebreselasie <yordibege@gmail.com>
2025-01-25 01:07:24 -0800

    Remove unused commented-out code

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,33 +1,12 @@
-// const pg = require("pg");
-
-// const client = new pg.Client({
-//   host: process.env.PGHOST,
-//   name: process.env.PGDATABASE,
-//   user: process.env.PGUSER,
-//   password: process.env.PGPASSWORD,
-//   port: process.env.PGPORT,
-//   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
-// });
-// client
-//   .connect()
-//   .then(() => console.log("Connected to PostgreSQL successfully!"))
-//   .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
-
-// module.exports = client;
-
-
-
-
-
 const { Client } = require("pg");
 
 const client = new Client({
-  host: process.env.PGHOST,                // Host from your .env
-  database: process.env.PGDATABASE,       // Database name
-  user: process.env.PGUSER,               // User from your .env
-  password: process.env.PGPASSWORD,       // Password
-  port: process.env.PGPORT,               // Port (default is 5432)
-  ssl: { rejectUnauthorized: false },     // Enforce SSL with relaxed certificate validation
+  host: process.env.PGHOST,                
+  database: process.env.PGDATABASE,      
+  user: process.env.PGUSER,               
+  password: process.env.PGPASSWORD,       
+  port: process.env.PGPORT,              
+  ssl: { rejectUnauthorized: false },    
 });
 
 client
